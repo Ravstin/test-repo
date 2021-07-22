51d0ae78dd577da5b30329e794b778f6a89ee35d
test-for-se2021 <84458811+test-for-se2021@users.noreply.github.com>
2021-07-22 17:30:18 +0300

    Add files via upload

---
+++ textreverse/index.js
@@ -0,0 +1,16 @@
+/**
+ * @return {string} reversed string
+ */
+function reverse(str) {
+    //TODO: Implement this function
+    return str;
+}
+
+/**********************************************/
+
+function init() {
+    const text = document.getElementById('text');
+    text.value = txt;
+}
+
+document.addEventListener('DOMContentLoaded', init);
